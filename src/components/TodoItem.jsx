1fe87cb637485409d57427938a0b3cfc9c330394
Muhammad Salif <muhammad.salif@yahoo.com>
2020-04-27 01:12:45 +0500

    Bootstrap changing done

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -4,13 +4,13 @@ class TodoItem extends Component {
   render() {
     const { title, handleDelete, handleEdit } = this.props;
     return (
-      <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
+      <li className="align-items-center text-center list-group-item text-capitalize d-flex justify-content-between my-2 ">
         <h6>{title}</h6>
-        <div className="todo-icon">
-          <span className="mx-2 text-success" onClick={handleEdit}>
+        <div className="todo-icon ">
+          <span className=" text-success btn btn-light" onClick={handleEdit}>
             <i className="fas fa-pen"></i>
           </span>
-          <span className="mx-2 text-danger" onClick={handleDelete}>
+          <span className=" text-danger btn btn-light" onClick={handleDelete}>
             <i className="fas fa-trash"></i>
           </span>
         </div>
